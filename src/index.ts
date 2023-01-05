cddcd3a56956f1e6c7bce982056f070d45379b7c
sophiebremer <44302338+sophiebremer@users.noreply.github.com>
2023-01-06 00:13:17 +0100

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+/* *
+ *
+ *  Imports
+ *
+ * */
+
+import JSON from'./JSON.js';
+
+/* *
+ *
+ *  Exports
+ *
+ * */
+
+export * from './JSON.js';
+
+/* *
+ *
+ *  Default Export
+ *
+ * */
+
+export default JSON;
